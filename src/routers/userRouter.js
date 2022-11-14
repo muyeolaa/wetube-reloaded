9d71107c2d8b233104f85456362dbe22290f6ddb
muyeolaa <115066625+muyeolaa@users.noreply.github.com>
2022-11-14 17:05:36 +0900

    logout

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,14 +1,10 @@
 import express from "express";
-import { edit,remove,logout,see, startGithubLogin,finishGithubLogin } from "../controlers/userController";
-
+import { edit, logout, see, startGithubLogin, finishGithubLogin,} from "../controlers/userController";
 const userRouter = express.Router();
 
-userRouter.get("/logout",logout);
-userRouter.get("/edit",edit);
-userRouter.get("/remove",remove);
+userRouter.get("/logout", logout);
+userRouter.get("/edit", edit);
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", finishGithubLogin);
-userRouter.get("/:id",see);
-
-
+userRouter.get(":id", see);
 export default userRouter;
