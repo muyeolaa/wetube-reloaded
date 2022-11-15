755abfbf2fb786cac6d6e366f01eca4467eaa9e7
muyeolaa <115066625+muyeolaa@users.noreply.github.com>
2022-11-15 12:11:00 +0900

    protect middleware

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { postEdit, logout, getEdit, see, startGithubLogin, finishGithubLogin,} from "../controlers/userController";
+import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
-userRouter.route("/edit").get(getEdit).post(postEdit);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+userRouter.get("/github/start",publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish",publicOnlyMiddleware, finishGithubLogin);
 userRouter.get(":id", see);
 export default userRouter;
