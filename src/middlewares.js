94f5ae39f178ac437356156aa22df1e6dcbb5861
muyeolaa <115066625+muyeolaa@users.noreply.github.com>
2022-11-15 12:13:27 +0900

    protect middleware
    2

--- src/middlewares.js
+++ src/middlewares.js
@@ -9,7 +9,7 @@ export const protectorMiddleware = (req,res,next) => {
   if(req.session.loggedIn){
     return     next();
     }else {
-        return res.redirect("/");
+     return res.redirect("/");
     }
 }; 
 
@@ -17,6 +17,6 @@ export const publicOnlyMiddleware = (req,res,next) => {
  if(!req.session.loggedIn){
     return    next();
     }else {
-        return res.redirect("/");
+    return res.redirect("/");
     }
 }; 
