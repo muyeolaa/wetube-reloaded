1f6fdd0064c0dd471c2d7ecf438d695d67b475c7
muyeolaa <115066625+muyeolaa@users.noreply.github.com>
2022-11-18 11:02:01 +0900

    User-Video_relationship

--- src/controlers/userController.js
+++ src/controlers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import Video from "../models/Video";
 import fetch from "node-fetch";
 import bcrypt from "bcrypt";
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
@@ -191,11 +192,11 @@ export const postChangePassword = async (req, res) => {
 
 export const see = async (req, res) => {
   const { id } = req.params;
-  const user = await User.findById(id);
+  const user = await User.findById(id).populate("videos");
+  console.log(user);
   if (!user) {
     return res.status(404).render("404", { pageTitle: "User not found." });
   }
-  console.log(user);
   return res.render("users/profile", {
     pageTitle: user.name,
     user,
