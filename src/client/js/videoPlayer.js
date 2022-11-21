88972623e334c4d87e427c1b43a95f2f18945e4e
muyeolaa <115066625+muyeolaa@users.noreply.github.com>
2022-11-21 18:24:19 +0900

    video player /Play,Pause

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,28 @@
-console.log("video player");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlayClick = (e) => {
+    // 비디오가 재생중이라면 멈추기
+    if(video.paused){
+        video.play();
+    } else {        
+        video.pause();
+    }
+    // 아나면 재생
+};
+
+const handlePause = () => (playBtn.innerText = "Play");
+const handlePlay = () => (playBtn.innerText = "Pause");
+
+
+const handleMute = (e) => {};
+
+
+playBtn.addEventListener("click",handlePlayClick);
+muteBtn.addEventListener("click",handleMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay); 
+
