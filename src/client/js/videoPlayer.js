1afc7db378858a700b933fea0215bc3095f84332
muyeolaa <115066625+muyeolaa@users.noreply.github.com>
2022-11-22 11:59:44 +0900

    videoPlayer make

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,28 +1,55 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
+
+let volumeValue= 0.5;
+video.value = volumeValue;
 
 const handlePlayClick = (e) => {
-    // 비디오가 재생중이라면 멈추기
     if(video.paused){
         video.play();
     } else {        
         video.pause();
     }
-    // 아나면 재생
+    playBtn.innerText = video.paused ? "Play" : "Pause"
 };
 
-const handlePause = () => (playBtn.innerText = "Play");
-const handlePlay = () => (playBtn.innerText = "Pause");
+const handleMute = (e) => {
+    if(video.muted){
+        video.muted = false;
+    }else {
+        video.muted = true;
+    }
+    muteBtn.innerText= video.muted ? "Unmute" : "Mute";
+    volumeRange.value = video.muted ? 0 : volumeValue ;
+};
 
+const handleVolumeChange = (event) => {
+    const { target :{ value }}=event;
+    if(video.muted){
+        video.muted = false;
+        muteBtn.innerText = "Mute";
+    }
+    volumeValue=value
+    video.volume=value;
+}
 
-const handleMute = (e) => {};
+const formatTime = (seconds) => 
+    new Date(seconds *1000).toISOString().substring(11,19);
+    
+const handleLoadedMetadata = () => {
+    totalTime.innerText = formatTime(Math.floor(video.duration));
+};
 
+const handleTimeUpdate = () => {
+    currentTime.innerText = formatTime(Math.floor(video.currentTime));
+};
 
 playBtn.addEventListener("click",handlePlayClick);
 muteBtn.addEventListener("click",handleMute);
-video.addEventListener("pause", handlePause);
-video.addEventListener("play", handlePlay); 
-
+volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata",handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
