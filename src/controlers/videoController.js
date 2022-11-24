d93af9fe69886eea217376a011b22c2db2bdc20e
muyeolaa <115066625+muyeolaa@users.noreply.github.com>
2022-11-24 17:02:32 +0900

    edit video bug fix

--- src/controlers/videoController.js
+++ src/controlers/videoController.js
@@ -38,11 +38,12 @@ export const postEdit = async (req, res) => {
   } = req.session;
   const { id } = req.params;
   const { title, description, hashtags } = req.body;
-  const video = await Video.exists({ _id: id });
+  const video = await Video.findById(id);
   if (!video) {
     return res.status(404).render("404", { pageTitle: "Video not found." });
   }
   if (String(video.owner) !== String(_id)) {
+    console.log(video.owner, _id);
     return res.status(403).redirect("/");
   }
   await Video.findByIdAndUpdate(id, {
